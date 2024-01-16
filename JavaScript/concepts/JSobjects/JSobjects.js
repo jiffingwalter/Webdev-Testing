2849c80950d80fb26660902da4f718ea65352b92
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-01-16 11:16:07 -0500

    Added arrays of objects testing

--- JavaScript/concepts/JSobjects/JSobjects.js
+++ JavaScript/concepts/JSobjects/JSobjects.js
@@ -51,3 +51,38 @@ console.log(`Nested object testing: Listing out ${person.name}'s file...`);
 for(const property in person){ // for loop to go through each property in an object
     console.log(person[property]);
 }
+
+// arrays of objects -----
+console.log('creating and modifying arrays of objects ----------------');
+class Fruit{
+    constructor(name,color,calories){
+        this.name = name;
+        this.color = color;
+        this.calories = calories;
+    }
+}
+
+const fruits = [new Fruit("apple","red",50),
+                new Fruit("orange","orange",67),
+                new Fruit("banana","yellow",62),
+                new Fruit("coconut","white",93),
+                new Fruit("pineapple","yellow",87)];
+
+for(const index in fruits){ // display all elments and their object properties
+    console.log(`name: ${fruits[index].name}, color: ${fruits[index].color}, calories: ${fruits[index].calories}`);
+}
+// filter and show only specific fruits
+const yellowFruits = fruits.filter(fruit => fruit.color === "yellow");
+const lowcalFruits = fruits.filter(fruit => fruit.calories <= 80);
+console.log(yellowFruits);
+console.log(lowcalFruits);
+
+// reduce and show a certain fruit
+const maxCals = fruits.reduce((max,fruit)=>
+    fruit.calories > max.calories ? fruit : max
+);
+console.log(maxCals); // show the fruit with the most calories
+const minCals = fruits.reduce((min,fruit)=>
+    fruit.calories < min.calories ? fruit : min
+);
+console.log(minCals); // show the fruit with the least calories
