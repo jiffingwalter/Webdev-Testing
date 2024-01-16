f606d5bb617c60113325b5c907d9561d6c0a2ed4
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-01-15 20:33:32 -0500

    JS classes work - getters and setters

--- JavaScript/concepts/JSclasses/JSclasses.js
+++ JavaScript/concepts/JSclasses/JSclasses.js
@@ -28,7 +28,7 @@ underwear.displayProduct();
 const total = shirt.calculateTotal(salesTax);
 console.log(`Total price after tax: $${total.toFixed(2)}`);
 
-console.log(`------------------------`)
+console.log(`static keyword testing ------------------------`)
 // static keyword testing - defines properties and methods only accessible to the class itself,
 //      rather than objects created from the class
 class MathUtil{
@@ -64,8 +64,8 @@ console.log(user1.username);
 console.log(`User count reported from object created from User class: ${user1.userCount} -- should be undefined!`);
 console.log(`User count reported directly from User class: ${User.userCount}`);
 
-console.log(`------------------------`)
-// inheritence and super keyword testing --------------------
+console.log(`inheritence and super keyword testing --------------------`)
+// --------------------------------------------------
 class Animal{
     name = '';
     age = 0;
@@ -139,3 +139,94 @@ console.log(`Is this ${hawk.name} alive? ${hawk.alive}`);
 hawk.eat();
 hawk.sleep();
 hawk.fly();
+
+// creating getters and setters --------------------------
+class Rectangle{
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+
+    set width(newWidth){
+        if (newWidth > 0){
+            this._width = newWidth; // underscores represent private properties in JSs
+        }
+        else {
+            console.error("Width must be a positive number");
+        }
+    }
+    get width(){
+        return this._width + `cm`;
+    }
+    set height(newHeight){
+        if (newHeight > 0){
+            this._height = newHeight;
+        }
+        else {
+            console.error("Height must be a positive number");
+        }
+    }
+    get height(){
+        return this._height + `cm`;
+    }
+
+    get area(){
+        return (this._width * this._height) + `cm^2`;
+    }
+}
+
+const rectangle = new Rectangle(-1000,'this is a string'); // would be bad if there was no validation. this can be done in a setter
+
+rectangle.width = 5;
+rectangle.height = 6;
+console.log(rectangle.area);
+
+// person example
+class Person{
+    constructor(firstname, lastname, age){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    set firstname(newFirstname){
+        if(typeof newFirstname === "string" && newFirstname.length > 0){
+            this._firstname = newFirstname;
+        } else {
+            console.error('First name must be a non-empty string');
+        }
+    }
+    set lastname(newLastname){
+        if(typeof newLastname === "string" && newLastname.length > 0){
+            this._lastname = newLastname;
+        } else {
+            console.error('Last name must be a non-empty string');
+        }
+    }
+    set age(newAge){
+        if(typeof newAge === "number" && newAge >= 0){
+            this._age = newAge;
+        } else {
+            console.error('Age must be a non-negative number');
+        }
+    }
+    get firstname(){
+        return this._firstname;
+    }
+    get lastname(){
+        return this._lastname;
+    }
+    get age(){
+        return this._age;
+    }
+    get fullname(){
+        return this._firstname + " " + this._lastname;
+    }
+}
+const personBad = new Person(420,69,'get punked'); // should not work
+const personGood = new Person('Simon','Douglas',28);
+
+console.log(personGood.firstname); // properties are not viewable by default since they're private and there's no getter
+console.log(personGood.lastname);
+console.log(personGood.age);
+console.log(personGood.fullname);
