a94ff9fa7f8cae732db959ea4d19e75df2a8e3a9
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-01-15 19:27:22 -0500

    Finish JS objects example, created JS classes files

--- JavaScript/concepts/JSobjects/JSobjects.js
+++ JavaScript/concepts/JSobjects/JSobjects.js
@@ -1,12 +1,35 @@
+// JavaScript objects - similar to a dictionary in other languages, JS objects are key:value collections of values and functions
+// must use "this" to access values inside the object like other languages
+//           -- note that "this" keyword DOESNT work inside functions made using arrow functions
+
 const person1 = {
     firstname: 'Jeff',
     lastname: 'Mynamis',
     age: 25,
     isEmployed: true,
-    sayHello: ()=>{
-        console.log('sup dawg');
+    sayHello: function(){
+        console.log(`sup dawg, ${this.lastname} ${this.firstname}`)
     }
 }
 
+// displaying information, calling an in-object function
 console.log(`Name: ${person1.lastname}, ${person1.firstname}`);
 person1.sayHello();
+
+// CONSTRUCTORS ----
+function User(username,password,email,age){
+    this.username = username,
+    this.password = password,
+    this.email = email,
+    this.age = age
+    this.getInfo = function(){
+        console.log(`New user's information made with a constructor:
+        Username: ${newUser.username}
+        Password: ${newUser.password}
+        Email: ${newUser.email}
+        Age: ${newUser.age}`);
+    }
+};
+
+const newUser = new User('justoni','superSecretPassword','justin@walter.net',25);
+newUser.getInfo();
