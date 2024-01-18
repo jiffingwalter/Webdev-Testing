9d1c95c44c8ca33d66a946cfaf76e104a81b85f3
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-01-18 12:32:57 -0500

    Added promise and async/await examples

--- JavaScript/concepts/async/async.js
+++ JavaScript/concepts/async/async.js
@@ -7,7 +7,7 @@ function func1(callback){
     setTimeout(()=> {
         console.log("Asynchronous task 1 finished...");
         callback();
-    },3000);
+    },5000);
 }
 
 function func2(){
@@ -17,3 +17,111 @@ function func2(){
 }
 
 func1(func2);
+
+// callback hell - idea in javascript of having many nested callbacks that gets hard to read
+function task1(callback){
+    setTimeout(()=>{
+        console.log('Task one finished');
+        callback();
+    }, 2000);
+}
+function task2(callback){
+    setTimeout(()=>{
+        console.log('Task two finished');
+        callback();
+    }, 1000);
+}
+function task3(callback){
+    setTimeout(()=>{
+        console.log('Task three finished');
+        callback();
+    }, 3000);
+}
+function task4(callback){
+    setTimeout(()=>{
+        console.log('Task four finished');
+        callback();
+    }, 1500);
+}
+
+task1(()=>{ // example of callback hell. it works but its gets ugly fast
+    task2(()=>{
+        task3(()=>{
+            task4(()=>{
+                console.log('All tasks complete!');
+            });
+        });
+    });
+});
+
+// PROMISES - objects that manage async operations. can wrap a promise object around an operation that returns
+//          if the operations was successful or failed, and does something based on either case
+// syntax:  new Promise((resolve, reject) => { - code to be ran - })
+
+// chores example
+function walkDog(){
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            const dogWalked = true;
+            if (dogWalked){
+                resolve("You walk the dog");
+            } else {
+                reject("You DIDN'T walk the dog");
+            }  
+        }, 1500);
+    });
+}
+function cleanKitchen(){
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            const kitchenCleaned = true;
+            if (kitchenCleaned){
+                resolve("You cleaned the kitchen");
+            } else {
+                reject("You DIDN'T clean the kitchen");
+            }  
+        }, 3000);
+    });
+}
+function takeOutTrash(){
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            const trashTakenOut = true;
+            if (trashTakenOut){
+                resolve("You took out the trash");
+            } else {
+                reject("You DIDN'T take out the trash");
+            }  
+        }, 500);
+    });
+}
+
+// entirely promise based implementation
+// walkDog().then(message=>{console.log(message); return cleanKitchen()})
+//          .then(message=>{console.log(message); return takeOutTrash()})
+//          .then(message=>{console.log(message); console.log("You finished all the chores!")})
+//          .catch(error=>console.error(error));
+
+// ASYNC / AWAIT keywords - allows you to write synchronous code in an async fashion. does not have resolve or reject parameters
+//      async = makes a function return a promise
+//      await = makes an async function WAIT for a promise. everything after await is placed in queue
+
+async function doChores(){
+    console.log('Running chores with async/await implementation...');
+    try{
+        const walkDogResult = await walkDog();
+        console.log(walkDogResult);
+
+        const cleanKitchenResult = await cleanKitchen();
+        console.log(cleanKitchenResult);
+
+        const takeOutTrashResult = await takeOutTrash();
+        console.log(takeOutTrashResult);
+
+        console.log("You finished all the chores!");
+    } catch (error){
+        console.error(error);
+    }
+}
+
+doChores();
