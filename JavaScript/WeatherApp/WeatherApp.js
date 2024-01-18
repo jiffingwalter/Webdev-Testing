3ad91ee4a487736ccbf18e9603159741f23c8b42
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-01-18 18:02:57 -0500

    Created git ignore, got rid of api key reference in project and imported it instead

--- JavaScript/WeatherApp/WeatherApp.js
+++ JavaScript/WeatherApp/WeatherApp.js
@@ -1,10 +1,11 @@
+import {apiKey} from './openweathermap_apikey.js';
+
 // weather app logic
-//initialization
+// initialization
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const errorDisplay = document.createElement("p");
-const apiKey = "a207b912f120defb578a314eafc5e045";
 
 weatherForm.addEventListener("submit", async event =>{
     event.preventDefault();
@@ -25,6 +26,7 @@ weatherForm.addEventListener("submit", async event =>{
     }
 })
 
+// call for the weather data from the openweathermap api
 async function getWeatherData(city){
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const response = await fetch(apiURL);
@@ -34,6 +36,7 @@ async function getWeatherData(city){
     return await response.json();
 }
 
+// create the weather card components and display the weather info on them
 function displayWeatherInfo(data){
     generateCard();
     const cityDisplay = document.querySelector(".cityDisplay");
@@ -56,21 +59,31 @@ function displayWeatherInfo(data){
     emojiDisplay.textContent = getWeatherEmoji(id);
 }
 
+// returns the appropriate emoji according to the current weather id, and also sets the background to the associated CSS background
 function getWeatherEmoji(weatherId){
+    const emojiContainer = document.querySelector(".card-col-emoji");
+    emojiContainer.className = "card-col-emoji"; // clear any bg classes that might have been applied already
     switch(true){
         case (weatherId >= 200 && weatherId < 300):
+            emojiContainer.classList.add("backgroundThunder");
             return "⛈️";
         case (weatherId >= 300 && weatherId < 400):
+            emojiContainer.classList.add("backgroundRainy");
             return "🌧️";
         case (weatherId >= 500 && weatherId < 600):
+            emojiContainer.classList.add("backgroundRainy");
             return "🌧️";
         case (weatherId >= 600 && weatherId < 700):
+            emojiContainer.classList.add("backgroundCloudy");
             return "❄️";
         case (weatherId >= 700 && weatherId < 800):
+            emojiContainer.classList.add("backgroundCloudy");
             return "🌫️";
         case (weatherId === 800):
+            emojiContainer.classList.add("backgroundSunny");
             return "☀️";
         case (weatherId >= 801 && weatherId < 810):
+            emojiContainer.classList.add("backgroundCloudy");
             return "☁️";
         default:
             return "❔";
@@ -78,6 +91,7 @@ function getWeatherEmoji(weatherId){
 }
 
 function displayError(message){
+    // remove error box on command
     if(message === "clear"){
         errorDisplay.style.display = "none";
         return;
