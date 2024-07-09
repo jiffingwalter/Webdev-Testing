da829aaf93aec37653451d554eab15fa058c6c02
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2024-07-09 18:25:33 -0400

    more typescript playing

--- TypeScript/TSandbox/template.js
+++ TypeScript/TSandbox/template.js
@@ -2,6 +2,31 @@
 var numbuh = 5;
 var word = 'bang';
 var bool = false;
+// values cannot change types
+// numbuh = 'five'; does NOT work
+// must initialize variables with explicit types with... 
+var character;
+var age;
+var isLoggedIn;
+// intializing arrays
+var numbers = [];
+numbers.push(1);
+// mixed arrays/union of types
+var mixed = [];
+mixed.push(1);
+mixed.push('hi');
+// mixed.push(true); -- invalid
+// functions must have their parameter types specifically defined
 var circ = function (diameter) {
     return diameter * Math.PI;
 };
+console.log(circ(10));
+// arrays are limited to whatever they are defined with initally (array of numbers must remain array of numbers, etc)
+// if initialized with mixed value types, can exist as such 
+// objects CANNOT have new keys added after initial creation
+var object;
+object = {
+    string: '10',
+    number: 10
+};
+console.log(object);
--- TypeScript/TSandbox/template.ts
+++ TypeScript/TSandbox/template.ts
@@ -2,7 +2,36 @@
 let numbuh = 5;
 let word = 'bang';
 let bool = false;
+// values cannot change types
+// numbuh = 'five'; does NOT work
 
+// must initialize variables with explicit types with... 
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+// intializing arrays
+let numbers: number[] = [];
+numbers.push(1);
+// mixed arrays/union of types
+let mixed: (string|number)[] = [];
+mixed.push(1);
+mixed.push('hi');
+// mixed.push(true); -- invalid
+
+// functions must have their parameter types specifically defined
 const circ = (diameter:number)=>{
     return diameter * Math.PI;
 };
+console.log(circ(10));
+
+// arrays are limited to whatever they are defined with initally (array of numbers must remain array of numbers, etc)
+// if initialized with mixed value types, can exist as such 
+
+// objects CANNOT have new keys added after initial creation
+let object: object;
+object = {
+    string: '10',
+    number: 10
+}
+console.log(object);
